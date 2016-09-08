310e6a81ca5a67afba5088c849c4c3197c4f0069
charlespcamargo <ch4rles6@gmail.com>
2016-09-08 19:41:37 -0300

    ...

---
+++ UFSCar.BD.BackEnd/Model/Helper/ArquivoVagasItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace UFSCar.BD.Model
+{
+    public class ArquivoVagasItem : IArquivoItem
+    {
+        public string DATA_GERACAO { get; set; }
+        public string HORA_GERACAO { get; set; }
+        public string ANO_ELEICAO { get; set; }
+        public string DESCRICAO_ELEICAO { get; set; }
+        public string SIGLA_UF { get; set; }
+        public string SIGLA_UE { get; set; }
+        public string NOME_UE { get; set; }
+        public string CODIGO_CARGO { get; set; }
+        public string DESCRICAO_CARGO { get; set; }
+        public string QTDE_VAGAS { get; set; }
+
+
+    }
+}
---
+++ UFSCar.BD.BackEnd/Model/ImportacaoBensCandidato.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFSCar.BD.Model
+{
+    [Table("ImportacaoBensCandidato")]
+    public class ImportacaoBensCandidato
+    {
+        [DataMember]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ID")]
+        public int ID { get; set; }
+
+        [Column("ImportacaoArquivoID")]
+        public int ImportacaoArquivoID { get; set; }
+
+        public string DATA_GERACAO { get; set; }
+
+        public string HORA_GERACAO { get; set; }
+
+        public string ANO_ELEICAO { get; set; }
+
+        public string DESCRICAO_ELEICAO { get; set; }
+
+        public string SIGLA_UF { get; set; }
+
+        public string SQ_CANDIDATO { get; set; }
+
+        public string CD_TIPO_BEM_CANDIDATO { get; set; }
+
+        public string DS_TIPO_BEM_CANDIDATO { get; set; }
+
+        public string DETALHE_BEM { get; set; }
+
+        public string VALOR_BEM { get; set; }
+
+        public string DATA_ULTIMA_ATUALIZACAO { get; set; }
+
+        public string HORA_ULTIMA_ATUALIZACAO { get; set; }
+    }
+}
