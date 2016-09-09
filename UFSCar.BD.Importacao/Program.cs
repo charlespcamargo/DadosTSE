963063957e3695bd72c316a039877f0b99670115
charlespcamargo <ch4rles6@gmail.com>
2016-09-08 23:19:05 -0300

    Renomeando diretórios...

---
+++ UFSCar.BD.Importacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportacaoDadosTSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArquivoHelper helper = ArquivoHelper.New;
+            helper.Iniciar();
+        }
+
+        
+
+
+    }
+}
