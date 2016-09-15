cbe06be190fe0c006321c2311b0dfac28886f684
charlespcamargo <ch4rles6@gmail.com>
2016-09-14 21:26:00 -0300

    ...

--- _scripts/consultas basicas.sql
+++ _scripts/consultas basicas.sql
@@ -1,8 +1,6 @@
- 
- SELECT TOP 10 *
-   FROM dbo.ImportacaoCandidato
-
-
+/*
+		
+*/
 SELECT Candidato.NOME_CANDIDATO, Candidato.SIGLA_PARTIDO, Tabela.Valor
   FROM ImportacaoCandidato	AS Candidato WITH(NOLOCK)				
   JOIN (
@@ -22,18 +20,19 @@ SELECT Candidato.NOME_CANDIDATO, Candidato.SIGLA_PARTIDO, Tabela.Valor
  ORDER BY Valor DESC
 
 
-   SELECT SIGLA_PARTIDO								AS Sigla,
-		  SUM(CAST(b.VALOR_BEM AS NUMERIC(14,2)))	AS Valor,
-		  COUNT(C.SEQUENCIAL_CANDIDATO)				AS Candidatos,
-		  AVG(CAST(b.VALOR_BEM AS NUMERIC(14,2)))	AS MediaPorCandidato
+SELECT SIGLA_PARTIDO								AS Sigla,
+		SUM(CAST(b.VALOR_BEM AS NUMERIC(14,2)))	AS Valor,
+		COUNT(C.SEQUENCIAL_CANDIDATO)				AS Candidatos,
+		AVG(CAST(b.VALOR_BEM AS NUMERIC(14,2)))	AS MediaPorCandidato
 
-	 FROM ImportacaoCandidato				AS C	    WITH(NOLOCK)				
-	 JOIN dbo.ImportacaoBensCandidato		AS B		WITH(NOLOCK)		
-	   ON C.SEQUENCIAL_CANDIDATO = B.SQ_CANDIDATO 
-	 WHERE C.SIGLA_UF = 'SP'
-	  AND C.NOME_MUNICIPIO_NASCIMENTO = 'SOROCABA'
-	GROUP BY SIGLA_PARTIDO
-	ORDER BY Valor DESC
+ FROM ImportacaoCandidato				AS C	    WITH(NOLOCK)				
+ JOIN dbo.ImportacaoBensCandidato		AS B		WITH(NOLOCK)		
+   ON C.SEQUENCIAL_CANDIDATO = B.SQ_CANDIDATO 
+WHERE C.SIGLA_UF = 'SP'
+  AND C.NOME_MUNICIPIO_NASCIMENTO = 'SOROCABA'
+GROUP BY SIGLA_PARTIDO
+ORDER BY Valor DESC
+
 
 /*
 	SELECT T.Nome, COUNT(A.ID) AS QtdArquivosImportados	
@@ -46,7 +45,4 @@ SELECT Candidato.NOME_CANDIDATO, Candidato.SIGLA_PARTIDO, Tabela.Valor
 	SELECT COUNT(ID) AS QtdRegistrosCandidato	FROM ImportacaoCandidato		WITH(NOLOCK)
 	SELECT COUNT(ID) AS QtdRegistrosLegendas	FROM ImportacaoLegenda			WITH(NOLOCK)
 	SELECT COUNT(ID) AS QtdRegistrosVagas		FROM ImportacaoVaga				WITH(NOLOCK)
-
-
-
 */
