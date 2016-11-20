822c8abc81f49611672acfe650a64d6c483c4be3
rodvieirasilva <rodvieirasilva@gmail.com>
2016-11-20 18:14:03 -0200

    [ADD] Criado script para primeira consulta

--- _scripts/CONSULTAS-ANALISES/1/1.1.sql
+++ _scripts/CONSULTAS-ANALISES/1/1.1.sql
@@ -6,7 +6,7 @@
 -- Create date: 20/11/2016
 -- Description:	Procedure para responder os dados da primeira Consulta
 -- =============================================
-CREATE PROCEDURE SP_ANALISE2_1(
+CREATE PROCEDURE SP_ANALISE1_1(
 	@ANO INT = NULL,
 	@SEXO VARCHAR(20) = NULL,
 	@IDESCOLARIDADE INT = NULL,
@@ -28,65 +28,53 @@ BEGIN
 	   TDLocalidade.Regiao, 
 	   TDLocalidade.SiglaEstado, 
 	   TDLocalidade.Municipio,
+	   TDCandidato.Nome,
+	   TDOcupacao.Descricao as Ocupacao,
+	   TDOcupacao.VlrMedioDeclarado as VlrMedioOcupacao,	   
+	   TFDadoEleitoral.VlrTotalDeclarado,
+	   (TFDadoEleitoral.VlrTotalDeclarado - TDOcupacao.VlrMedioDeclarado) AS DiferencaMedia ,
 	   TDCargoPolitico.Descricao as CargoPolitico,
-	   TDPartidoColigacao.Partido,
-	   TDEscolaridade.Descricao AS Escolaridade,
-	   COUNT(1) AS Quantidade,
-	   AG_ANO_LOCAL_CARGO_PARTIDO.QtdCandidatos as Total,
-	   (COUNT(1) / (AG_ANO_LOCAL_CARGO_PARTIDO.QtdCandidatos + 0.0)) * 100.0 AS Percentual,
-	   (RIGHT('000' + CAST(COUNT(1) AS VARCHAR(20)), 3) + ' de ' + RIGHT('000' + CAST(AG_ANO_LOCAL_CARGO_PARTIDO.QtdCandidatos AS VARCHAR(20)), 3) ) AS Fracao
+	   TDPartidoColigacao.Partido
 	   
-
-
-	    FROM TFDadoEleitoral
+	FROM TFDadoEleitoral
 		INNER JOIN TPCandidatoEscolaridade ON TFDadoEleitoral.CandidatoEscolaridadeID = TPCandidatoEscolaridade.ID
 		INNER JOIN TDCandidato ON TPCandidatoEscolaridade.CandidatoID = TDCandidato.ID
-		INNER JOIN TDEscolaridade ON TPCandidatoEscolaridade.EscolaridadeID = TDEscolaridade.ID
 		INNER JOIN TDCargoPolitico ON TFDadoEleitoral.CargoPoliticoID = TDCargoPolitico.ID
 		INNER JOIN TDLocalidade ON TFDadoEleitoral.LocalidadeID = TDLocalidade.ID
 		INNER JOIN TDOcupacao ON TFDadoEleitoral.OcupacaoID = TDOcupacao.ID
 		INNER JOIN TDPartidoColigacao ON TFDadoEleitoral.PartidoColigacaoID = TDPartidoColigacao.ID
-		INNER JOIN AG_ANO_LOCAL_CARGO_PARTIDO ON TFDadoEleitoral.Ano = AG_ANO_LOCAL_CARGO_PARTIDO.ANO AND 
-			   TDLocalidade.ID = AG_ANO_LOCAL_CARGO_PARTIDO.LocalidadeID AND
-			   TDCargoPolitico.ID = AG_ANO_LOCAL_CARGO_PARTIDO.CargoPoliticoID AND
-			   TDPartidoColigacao.ID = AG_ANO_LOCAL_CARGO_PARTIDO.PartidoColigacaoID
 		WHERE 	
-				ISNULL(@ANO,TFDadoEleitoral.Ano) = TFDadoEleitoral.Ano
-			AND 
-				ISNULL(@SEXO,TDCandidato.Sexo) = TDCandidato.Sexo		
-			AND 
-				ISNULL(@IDESCOLARIDADE,TPCandidatoEscolaridade.EscolaridadeID) = TPCandidatoEscolaridade.EscolaridadeID
-			AND 
-				ISNULL(@IDOCUPACAO,TDOcupacao.ID) = TDOcupacao.ID
-			AND 
-				ISNULL(@REGIAO,TDLocalidade.Regiao) = TDLocalidade.Regiao
-			AND 
-				ISNULL(@SIGLAESTADO,TDLocalidade.SiglaEstado) = TDLocalidade.SiglaEstado
-			AND 
-				ISNULL(@IDMUNICIPIO,TDLocalidade.ID) = TDLocalidade.ID
-			AND 
-				ISNULL(@SIGLAPARTIDO,TDPartidoColigacao.Partido) = TDPartidoColigacao.Partido
-			AND 
-				ISNULL(@IDCARGOPRETENDIDO,TDCargoPolitico.ID) = TDCargoPolitico.ID
+			--	ISNULL(@ANO,TFDadoEleitoral.Ano) = TFDadoEleitoral.Ano
+			--AND 
+			--	ISNULL(@SEXO,TDCandidato.Sexo) = TDCandidato.Sexo		
+			--AND 
+			--	ISNULL(@IDESCOLARIDADE,TPCandidatoEscolaridade.EscolaridadeID) = TPCandidatoEscolaridade.EscolaridadeID
+			--AND 
+			--	ISNULL(@IDOCUPACAO,TDOcupacao.ID) = TDOcupacao.ID
+			--AND 
+			--	ISNULL(@REGIAO,TDLocalidade.Regiao) = TDLocalidade.Regiao
+			--AND 
+			--	ISNULL(@SIGLAESTADO,TDLocalidade.SiglaEstado) = TDLocalidade.SiglaEstado
+			--AND 
+			--	ISNULL(@IDMUNICIPIO,TDLocalidade.ID) = TDLocalidade.ID
+			--AND 
+			--	ISNULL(@SIGLAPARTIDO,TDPartidoColigacao.Partido) = TDPartidoColigacao.Partido
+			--AND 
+			--	ISNULL(@IDCARGOPRETENDIDO,TDCargoPolitico.ID) = TDCargoPolitico.ID
 
-		--TDPartidoColigacao.Partido = 'PT' AND TDCargoPolitico.Descricao = 'VEREADOR' AND Municipio = 'RIO DE JANEIRO'
+		TDPartidoColigacao.Partido = 'PT' AND TDCargoPolitico.Descricao = 'VEREADOR' AND Municipio = 'RIO DE JANEIRO'
 
-		GROUP BY TFDadoEleitoral.Ano, 
-			   TDLocalidade.Regiao, 
-			   TDLocalidade.SiglaEstado, 
-			   TDLocalidade.Municipio,
-			   TDCargoPolitico.Descricao,
-			   TDPartidoColigacao.Partido,
-			   AG_ANO_LOCAL_CARGO_PARTIDO.QtdCandidatos,
-			   TDEscolaridade.Descricao,
-			   TDEscolaridade.Nivel
+		--GROUP BY TFDadoEleitoral.Ano, 
+		--	   TDLocalidade.Regiao, 
+		--	   TDLocalidade.SiglaEstado, 
+		--	   TDLocalidade.Municipio,
+		--	   TDCargoPolitico.Descricao,
+		--	   TDPartidoColigacao.Partido
 		ORDER BY TFDadoEleitoral.Ano, 
 			   TDLocalidade.Regiao, 
 			   TDLocalidade.SiglaEstado, 
 			   TDLocalidade.Municipio,
-			   TDCargoPolitico.Descricao,
-			   TDPartidoColigacao.Partido,
-			   TDEscolaridade.Nivel
+			   DiferencaMedia DESC
 END
 --GO
 
