7479946319f74c38fbc129802fb6ac1af32af3bf
charlespcamargo <ch4rles6@gmail.com>
2016-11-20 21:47:16 -0200

    relatórios

---
+++ UFSCar.BD.BackEnd/Model/Consultas/ANALISE1_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFSCar.BD.BackEnd.Model
+{
+    public class ANALISE1_1
+    {
+        public int Ano { get; set; }
+
+        public string Regiao { get; set; }
+
+        public string SiglaEstado { get; set; }
+
+        public string Municipio { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Ocupacao { get; set; }
+
+        public string VlrMedioOcupacao { get; set; }
+
+        public string VlrTotalDeclarado { get; set; }
+
+        public string DiferencaMedia { get; set; }
+
+        public string CargoPolitico { get; set; }
+
+        public string Partido { get; set; }
+
+    }
+}
