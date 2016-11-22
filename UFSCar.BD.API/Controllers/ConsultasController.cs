6500cb590d51df5c4cbde4f221d4cd6b05506a68
charlespcamargo <ch4rles6@gmail.com>
2016-11-22 20:24:24 -0200

    gráficos..

--- UFSCar.BD.API/Controllers/ConsultasController.cs
+++ UFSCar.BD.API/Controllers/ConsultasController.cs
@@ -49,9 +49,9 @@ namespace UFSCar.BD.API.Controllers
         {
             try
             {
-                lst = ConsultasBL.New.EvolucaoPatrimonialGrafico(cpf);
+                //lst = ConsultasBL.New.EvolucaoPatrimonialGrafico(cpf);
 
-                return Request.CreateResponse(HttpStatusCode.OK, lst);
+                return Request.CreateResponse(HttpStatusCode.OK);
             }
             catch (ArgumentException aex)
             {
