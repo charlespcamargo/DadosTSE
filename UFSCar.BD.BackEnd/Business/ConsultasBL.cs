c7773ea9b6ff29c03736c6354f48b527b810efa9
charlespcamargo <ch4rles6@gmail.com>
2016-11-22 19:18:42 -0200

    gráficos..

--- UFSCar.BD.BackEnd/Business/ConsultasBL.cs
+++ UFSCar.BD.BackEnd/Business/ConsultasBL.cs
@@ -17,10 +17,9 @@ namespace UFSCar.BD.BackEnd.Business
         }
 
 
-        public ANALISE1_1_RELATORIO EvolucaoPatrimonial(AnaliseFiltro filtro)
+        public List<ANALISE1_1> EvolucaoPatrimonial(AnaliseFiltro filtro)
         {
-            List<ANALISE1_1> lst = null;
-            ANALISE1_1_RELATORIO relatorio = new ANALISE1_1_RELATORIO();
+            List<ANALISE1_1> lst = null; 
 
             #region Parametros
 
@@ -106,103 +105,11 @@ namespace UFSCar.BD.BackEnd.Business
                 string script = "SP_ANALISE1_1  @ANO, @TODOSANOS, @SEXO, @IDESCOLARIDADE, @IDOCUPACAO, @REGIAO, @SIGLAESTADO, @IDMUNICIPIO, @SIGLAPARTIDO, @IDCARGOPRETENDIDO ";
 
                 lst = UoW.GetContext().Database.SqlQuery<ANALISE1_1>(script, lstParametros.ToArray()).ToList();
-            }
+            }              
 
-
-            List<ANALISE1_1> lstRelatorio = new List<ANALISE1_1>();
-
-            if (lst != null && lst.Count > 0)
-            {
-                List<ANALISE1_1> candidatos = lst.GroupBy(group => new { group.CPF }).
-                                                         Select(unico => new ANALISE1_1()
-                                                         {
-                                                             CPF = unico.Key.CPF
-                                                         }).ToList();
-
-
-
-                Model.ANALISE1_1_LINHA linha = new Model.ANALISE1_1_LINHA();
-                linha.lstColuna.Add(new ANALISE1_1_COLUNA("ANO"));
-
-                foreach (ANALISE1_1 unico in candidatos)
-                {
-                    PreencherNomeOcupacao(linha, lst, unico, filtro.Ano);
-                }
-                relatorio.lstLinha.Add(linha);
-
-                List<int> lstAnos = new List<int>();
-                lstAnos.Add(2006);
-                lstAnos.Add(2008);
-                lstAnos.Add(2010);
-                lstAnos.Add(2012);
-                lstAnos.Add(2014);
-                lstAnos.Add(2016);
-
-                foreach (int ano in lstAnos)
-                {
-                    linha = new Model.ANALISE1_1_LINHA();
-                    linha.lstColuna.Add(new ANALISE1_1_COLUNA(ano.ToString()));
-
-                    foreach (ANALISE1_1 unico in candidatos)
-                    {
-                        Preencher(linha, lst, unico, ano);
-                    }
-
-                    relatorio.lstLinha.Add(linha);
-                }
-
-
-            }
-
-            /*
-                [
-                    ['Ano', 'C1', 'C2', 'C3', 'C1M', 'c2M', 'c3M'],
-                    ['2010', 8175000, 8008000, 8008000, 4000000, 3000000, 2000000],
-                    ['2011', 3792000, 3694000, 3694000, 4000000, 3000000, 2000000],
-                    ['2012', 2695000, 2896000, 2896000, 4000000, 3000000, 2000000],
-                    ['2014', 2099000, 1953000, 1953000, 4000000, 3000000, 2000000]
-                ]
-            */
-
-            return relatorio;
+            return lst;
         }
-
-        public void Preencher(Model.ANALISE1_1_LINHA linha, List<ANALISE1_1> lst, ANALISE1_1 unico, int ano)
-        {
-            ANALISE1_1 encontrarAno = lst.FirstOrDefault(w => w.CPF == unico.CPF && w.Ano == ano);
-            if (encontrarAno != null)
-            {
-                linha.lstColuna.Add(new ANALISE1_1_COLUNA(encontrarAno.VlrTotalDeclarado.ToString()));
-                linha.lstColuna.Add(new ANALISE1_1_COLUNA(encontrarAno.VlrMedioOcupacao.ToString()));
-            }
-            else
-            {
-                linha.lstColuna.Add(new ANALISE1_1_COLUNA("0"));
-                linha.lstColuna.Add(new ANALISE1_1_COLUNA("0"));
-            }
-        }
-
-        public void PreencherNomeOcupacao(Model.ANALISE1_1_LINHA linha, List<ANALISE1_1> lst, ANALISE1_1 unico, int ano)
-        {
-            ANALISE1_1 encontrarAno = lst.FirstOrDefault(w => w.CPF == unico.CPF && w.Ano == ano);
-            if (encontrarAno != null)
-            {
-                linha.lstColuna.Add(new ANALISE1_1_COLUNA(encontrarAno.Nome));
-                linha.lstColuna.Add(new ANALISE1_1_COLUNA(encontrarAno.Ocupacao));
-            }
-        }
-
-        public void Preencher(List<ANALISE1_1> lst, ANALISE1_1 entidade, ANALISE1_1 unico, int ano, int i)
-        {
-            ANALISE1_1 encontrarAno = lst.FirstOrDefault(w => w.CPF == unico.CPF && w.Ano == ano);
-            if (encontrarAno != null)
-            {
-                entidade.lstVlrMedioOcupacao[i] = encontrarAno.VlrMedioOcupacao;
-                entidade.lstVlrTotalDeclarado[i] = encontrarAno.VlrTotalDeclarado;
-            }
-        }
-
-
+                
         public List<ANALISE3_2> PorSexo(AnaliseFiltro filtro)
         {
             List<ANALISE3_2> lst = null;
