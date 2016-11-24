34e85f7beedc7c51160744455e5d4873dda05484
charlespcamargo <ch4rles6@gmail.com>
2016-11-23 23:35:59 -0200

    Relátório de Escolaridade.

---
+++ UFSCar.BD.BackEnd/Model/Consultas/ANALISE2_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFSCar.BD.BackEnd.Model
+{
+    public class ANALISE2_1
+    {
+
+        public int Ano { get; set; }
+        public string Regiao { get; set; }
+        public string SiglaEstado { get; set; }
+        public string Municipio { get; set; }
+        public string CargoPolitico { get; set; }
+        public string Partido { get; set; }
+        public string Escolaridade { get; set; }
+        public int Quantidade { get; set; }
+        public int Total { get; set; }
+        public decimal Percentual { get; set; }
+        public string Fracao { get; set; }
+
+    }
+
+}
